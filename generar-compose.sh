cabd7f7bae13a59f84eb9857ac9eaee6c934bb6c
axel-zielonka <azielonka@fi.uba.ar>
2025-08-27 12:19:16 -0300

    Modificación del error devuelto en caso de numero de clientes invalido

--- generar-compose.sh
+++ generar-compose.sh
@@ -11,7 +11,7 @@ NUM_CLIENTS="$2"
 
 if ! [[ "$NUM_CLIENTS" =~ ^[0-9]+$ ]] || [[ "$NUM_CLIENTS" -lt 1 ]]; then
   echo "Error:  debe ser un entero >= 1"
-  exit 1
+  exit 2
 fi
 
 cat > "$OUTPUT_FILE" <<'END'
